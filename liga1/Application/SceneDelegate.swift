5285ad1ae5ee579607099e23381ea410f317c87f
Miguel Angel Tomairo Mendez <e_mtomairo@smu.cl>
2025-09-13 20:33:26 -0500

    feat:MT-04 refactor AppDelegate and MainTabBar

---
+++ liga1/Application/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  liga1
+//
+//  Created by miguel tomairo on 15/08/24.
+//
+
+import UIKit
+import FirebaseAuth
+import GoogleSignIn
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        // Configurar singletons
+        AppRouter.shared.configure(window: window)
+        SessionManager.shared.configure(with: window)
+        
+        // Mostrar pantalla inicial
+        AppRouter.shared.setInitialViewController()
+        window.makeKeyAndVisible()
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            GIDSignIn.sharedInstance.handle(url)
+        }
+    }
+}
+
